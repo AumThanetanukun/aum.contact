49ec3bd2d825b20ed5be642fab654beb8b090457
Aum Thanetanukun <aum.thanetanukun@gmail.com>
2023-10-04 00:46:25 +0700

    Setup packages and nuget

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,13 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddDbContext<QuoteContext>(opt =>
+    opt.UseInMemoryDatabase("QuoteList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,6 +18,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
