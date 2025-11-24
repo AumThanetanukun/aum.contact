9663fb8d73f8a47ba29a3b8938bebcf022f57bae
Aum Thanetanukun <aum.thanetanukun@gmail.com>
2025-11-24 22:46:00 +0700

    Remove project.nuget.cache and README.md files as part of project cleanup

--- Backend/Models/QuoteItem.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.Models;
-
-public class QuoteItem
-{
-    public long Id { get; set; }
-    public string? Passage { get; set; }
-    public string? Writer { get; set; }
-    public string? Lesson { get; set; }
-}
